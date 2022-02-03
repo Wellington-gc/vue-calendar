dee004c6dcdbef7f84ca323b78e59708197ee40b
Wellington Alves <wellington_gc@outlook.com>
2022-02-03 20:29:46 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <calendar/>
+</template>
+
+<script>
+import Calendar from './components/Calendar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Georgia, "Times New Roman", Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
